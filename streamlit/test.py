e89aad8e0606f051d3b38500b7926adef17177dd
Maki <haru.maki.ch.0303@gmail.com>
2024-03-07 23:01:47 +0900

    Create test.py

---
+++ streamlit/test.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from audio_recorder_streamlit import audio_recorder
+from datetime import datetime
+from pathlib import Path
+import os
+
+
+
+# Example usage with Streamlit:
+def main():
+    st.title("Voice to Text Transcription 2")
+    
+if __name__ == "__main__":
+    main()
